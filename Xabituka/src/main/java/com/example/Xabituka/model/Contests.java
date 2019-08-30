365a7188d8a85ea556f6f90720896ad7d91902f4
batatavf2 <viniciusf.006@gmail.com>
2019-08-30 12:26:56 -0300

    Added Contests Model

--- Xabituka/src/main/java/com/example/Xabituka/model/Contests.java
+++ Xabituka/src/main/java/com/example/Xabituka/model/Contests.java
@@ -4,10 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Set;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,4 +18,7 @@ public class Contests {
 
     private String name;
     private boolean open;
+
+    @OneToMany(mappedBy = "contest", cascade = CascadeType.ALL)
+    private Set<ContestsQuestions> contestsQuestions;
 }
---
+++ Xabituka/src/main/java/com/example/Xabituka/model/ContestsQuestions.java
@@ -0,0 +1,25 @@
+package com.example.Xabituka.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Data
+public class ContestsQuestions {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String question;
+    private Boolean answer;
+
+    @ManyToOne
+    @JoinColumn
+    private Contests contest;
+}
