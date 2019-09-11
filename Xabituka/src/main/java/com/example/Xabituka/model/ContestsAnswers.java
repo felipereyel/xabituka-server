5cc3f4fa55e1ca6fa342aae8aeef1d28cbfb7bd3
batatavf2 <viniciusf.006@gmail.com>
2019-09-11 12:23:45 -0300

    Created ContestsAnswers and LearnAnswers models

---
+++ Xabituka/src/main/java/com/example/Xabituka/model/ContestsAnswers.java
@@ -0,0 +1,22 @@
+package com.example.Xabituka.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Data
+public class ContestsAnswers {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private boolean gotItRight;
+    private Long userId;
+    private Long contestId;
+}
