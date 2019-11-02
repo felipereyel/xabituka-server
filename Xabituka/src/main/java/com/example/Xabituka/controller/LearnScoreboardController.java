71228d0be8c0371fff85dfd0e4c795b9a20571c6
batatavf2 <viniciusf.006@gmail.com>
2019-11-02 18:15:43 -0300

    honestly, i dont know

--- Xabituka/src/main/java/com/example/Xabituka/controller/LearnScoreboardController.java
+++ Xabituka/src/main/java/com/example/Xabituka/controller/LearnScoreboardController.java
@@ -2,12 +2,12 @@ package com.example.Xabituka.controller;
 
 import com.example.Xabituka.model.LearnScoreboard;
 import com.example.Xabituka.repository.LearnScoreboardRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping({"/learn/scoreboard"})
 public class LearnScoreboardController {
 
     private LearnScoreboardRepository repository;
