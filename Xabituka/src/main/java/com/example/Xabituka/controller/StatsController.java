7b1a3f8a81244fb36ede282cfde23e26f857389b
batatavf2 <viniciusf.006@gmail.com>
2019-11-16 14:59:48 -0300

    stats

--- Xabituka/src/main/java/com/example/Xabituka/controller/StatsController.java
+++ Xabituka/src/main/java/com/example/Xabituka/controller/StatsController.java
@@ -1,19 +1,15 @@
 package com.example.Xabituka.controller;
 
-import com.example.Xabituka.model.LearnAnswers;
-import com.example.Xabituka.model.LearnQuestions;
-import com.example.Xabituka.model.Subjects;
-import com.example.Xabituka.model.Topics;
-import com.example.Xabituka.repository.LearnAnswersRepository;
-import com.example.Xabituka.repository.LearnQuestionsRepository;
-import com.example.Xabituka.repository.SubjectsRepository;
-import com.example.Xabituka.repository.TopicsRepository;
+import com.example.Xabituka.model.*;
+import com.example.Xabituka.repository.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.security.auth.Subject;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -28,90 +24,94 @@ public class StatsController {
     private TopicsRepository topicsRepository;
     private LearnAnswersRepository learnAnswersRepository;
     private SubjectsRepository subjectsRepository;
+    private UsersRepository usersRepository;
+
 
     public StatsController(
             LearnQuestionsRepository learnQuestionsRepository,
             TopicsRepository topicsRepository,
             LearnAnswersRepository learnAnswersRepository,
-            SubjectsRepository subjectsRepository
+            SubjectsRepository subjectsRepository,
+            UsersRepository usersRepository
     ) {
         this.learnQuestionsRepository = learnQuestionsRepository;
         this.topicsRepository = topicsRepository;
         this.learnAnswersRepository = learnAnswersRepository;
         this.subjectsRepository = subjectsRepository;
+        this.usersRepository = usersRepository;
     }
 
+    @GetMapping({"/{subjectId}/{userId}"})
+    public LinkedHashMap findBySubjectId(@PathVariable long subjectId, @PathVariable long userId) {
 
-//    @GetMapping
-//    public List findAll() {
-//        return repository.findAll();
-//    }
+        return getUserStatsBySubjectId(userId, subjectId);
+    }
 
-    @GetMapping({"/{userId}"})
-    public List<LinkedHashMap> findBySubjectId(@PathVariable long userId) {
+    private LinkedHashMap getUserStatsBySubjectId(long userId, Long subjectId) {
+        LinkedHashMap subjectStats = new LinkedHashMap();
+        List<Subjects> subjects = subjectsRepository.findAllById(Collections.singleton(subjectId));
 
-        List<LinkedHashMap> stats = new ArrayList<>();
-        List<Subjects> subjects = subjectsRepository.findAll();
-
-        for (Subjects subject : subjects) {
-            LinkedHashMap subjectStats = new LinkedHashMap();
-            List<LinkedHashMap> topics = new ArrayList<>();
-            int numberOfAnswers = 0;
-            int numberOfCorrectAnswers = 0;
-
-            subjectStats.put("subjectId", subject.getId());
-            subjectStats.put("subjectName", subject.getName());
-
-            //calcular stats por topico
-            List<Topics> topicsModel = topicsRepository.findBySubjectId(subject.getId());
-            for (Topics topicModel : topicsModel) {
-                LinkedHashMap topicStats = new LinkedHashMap();
-                topicStats.put("topicId", topicModel.getId());
-                topicStats.put("topicName", topicModel.getName());
-
-                int numberOfTopicAnswers = 0;
-                int numberOfCorrectTopicAnswers = 0;
-
-                List<LearnQuestions> topicQuestions = learnQuestionsRepository.findByTopicId(topicModel.getId());
-
-                List<Long> topicQuestionsIds = topicQuestions
-                        .stream()
-                        .map(it -> it.getId())
-                        .collect(Collectors.toList());
-
-                List<LearnAnswers> topicAnswers = learnAnswersRepository.findAll()
-                        .stream()
-                        .filter(it -> it.getUserId() == userId)
-                        .filter(it -> topicQuestionsIds.contains(it.getLearnQuestionsId()))
-                        .collect(Collectors.toList());
-
-                final List<LinkedHashMap> joinado = JoinQuestions(topicQuestions, topicAnswers);
-
-                for(LinkedHashMap joinadinho: joinado){
-                    if(joinadinho.containsKey("answer"))
-                    {
-                        numberOfTopicAnswers++;
-                        numberOfAnswers++;
-                        if((Boolean) joinadinho.get("answer"))
-                        {
-                            numberOfCorrectTopicAnswers++;
-                            numberOfCorrectAnswers++;
-                        }
-                    }
-                }
-
-                topicStats.put("numberOfTopicAnswers", numberOfTopicAnswers);
-                topicStats.put("numberOfCorrectTopicAnswers", numberOfCorrectTopicAnswers);
-                topics.add(topicStats);
-            }
-
-            subjectStats.put("topics", topics);
-            subjectStats.put("numberOfAnswers", numberOfAnswers);
-            subjectStats.put("numberOfCorrectAnswers", numberOfCorrectAnswers);
-            stats.add(subjectStats);
+        if(subjects.isEmpty()){
+            return subjectStats;
         }
 
-        return stats;
+        Subjects subject = subjects.get(0);
+
+        List<LinkedHashMap> topics = new ArrayList<>();
+        int numberOfAnswers = 0;
+        int numberOfCorrectAnswers = 0;
+
+        //calcular stats por topico
+        List<Topics> topicsModel = topicsRepository.findBySubjectId(subject.getId());
+        for (Topics topicModel : topicsModel) {
+            int numberOfTopicAnswers = 0;
+            int numberOfCorrectTopicAnswers = 0;
+
+            List<LearnQuestions> topicQuestions = learnQuestionsRepository.findByTopicId(topicModel.getId());
+
+            List<Long> topicQuestionsIds = topicQuestions
+                    .stream()
+                    .map(it -> it.getId())
+                    .collect(Collectors.toList());
+
+            List<LearnAnswers> topicAnswers = learnAnswersRepository.findAll()
+                    .stream()
+                    .filter(it -> it.getUserId() == userId)
+                    .filter(it -> topicQuestionsIds.contains(it.getLearnQuestionsId()))
+                    .collect(Collectors.toList());
+
+            final List<LinkedHashMap> joinado = JoinQuestions(topicQuestions, topicAnswers);
+
+            for(LinkedHashMap joinadinho: joinado){
+                if(joinadinho.containsKey("answer"))
+                {
+                    numberOfTopicAnswers++;
+                    numberOfAnswers++;
+                    if((Boolean) joinadinho.get("answer"))
+                    {
+                        numberOfCorrectTopicAnswers++;
+                        numberOfCorrectAnswers++;
+                    }
+                }
+            }
+
+            if(numberOfTopicAnswers == 0) continue;
+
+            LinkedHashMap topicStats = new LinkedHashMap();
+            topicStats.put("topicId", topicModel.getId());
+            topicStats.put("topicName", topicModel.getName());
+            topicStats.put("numberOfTopicAnswers", numberOfTopicAnswers);
+            topicStats.put("numberOfCorrectTopicAnswers", numberOfCorrectTopicAnswers);
+            topics.add(topicStats);
+        }
+
+        subjectStats.put("subjectId", subject.getId());
+        subjectStats.put("subjectName", subject.getName());
+        subjectStats.put("topics", topics);
+        subjectStats.put("numberOfAnswers", numberOfAnswers);
+        subjectStats.put("numberOfCorrectAnswers", numberOfCorrectAnswers);
+
+        return subjectStats;
     }
 }
 
--- Xabituka/src/main/java/com/example/Xabituka/repository/UsersRepository.java
+++ Xabituka/src/main/java/com/example/Xabituka/repository/UsersRepository.java
@@ -4,8 +4,11 @@ import com.example.Xabituka.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
     Users findByNickname(String nickname);
+    List <Users> findByUserType(String userType);
 }
